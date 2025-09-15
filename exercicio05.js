7ce7e78415d25fac6917d8efe053dd7de6bcb507
Vitoria-Araujo <vitoria.araujo1@hotmail.com git config --global user.name Vitoria-Araujogit statusgit config --global user.email vitoria.araujo1@hotmail.comgit config --global user.email vitoria.araujo1@hotmail.com>
2025-09-15 10:40:13 -0300

    Exercício 5:Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.

---
+++ exercicio05.js
@@ -0,0 +1,8 @@
+//Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.
+
+let num1 = 50;
+let num2 = 20;
+
+let soma = num1 + num2;
+
+console.log(soma)
