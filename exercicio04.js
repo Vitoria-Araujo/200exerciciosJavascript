e1c941046d525a0758fcf441c549a6e15e6a7eae
Vitória <104187728+Vitoria-Araujo@users.noreply.github.com>
2025-09-16 20:41:06 -0300

    Exercício 4: Declare duas variáveis chamadas 'nome' e 'sobrenome'

---
+++ exercicio04.js
@@ -0,0 +1,6 @@
+//Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+let nome = 'Vitória ';
+let sobrenome = 'Araujo';
+let nomeCompleto = nome + sobrenome;
+
+console.log(`Seu nome completo é ${nomeCompleto}`);
