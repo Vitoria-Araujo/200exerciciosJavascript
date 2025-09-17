d369e9a1657422a8aa259be300b8c12c71790700
Vitória <104187728+Vitoria-Araujo@users.noreply.github.com>
2025-09-16 22:22:48 -0300

    Exercício 6: Declare duas variáveis e atribua valores booleanos a elas. Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis. Imprima os resultados.

---
+++ exercicio06.js
@@ -0,0 +1,10 @@
+//Exercício 6: Declare duas variáveis e atribua valores booleanos a elas.
+//  Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis.
+// Imprima os resultados.
+let positivo = true;
+let negativo = false;
+
+console.log(`And: ${positivo && negativo}`);
+console.log(`Or: ${positivo || negativo}`);
+console.log(`Not: ${!positivo}`);
+
