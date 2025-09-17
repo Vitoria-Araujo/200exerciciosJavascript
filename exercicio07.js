b6c5014581631ba1653ec51dfb090a9edba7ca90
Vitória <104187728+Vitoria-Araujo@users.noreply.github.com>
2025-09-16 22:23:24 -0300

    Exercício 7: Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.

---
+++ exercicio07.js
@@ -0,0 +1,12 @@
+/*Exercício 7: Declare três variáveis e atribua valores numéricos a elas.
+ Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.*/
+ let num1 = 10;
+ let num2 = 20;
+ let num3 = 10;
+
+    console.log(`num1 é maior que num2? ${num1 > num2}`);
+    console.log(`num1 é menor que num2? ${num1 < num2}`);
+    console.log(`num1 é igual a num3? ${num1 === num3}`);
+    console.log(`num2 é diferente de num3? ${num2 !== num3}`);
+    console.log(`num1 é maior ou igual a num3? ${num1 >= num3}`);
+    console.log(`num2 é menor ou igual a num1? ${num2 <= num1}`);
