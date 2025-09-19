6f0263e84a1fecf717212f1799aca5fc504abf54
Vitória <104187728+Vitoria-Araujo@users.noreply.github.com>
2025-09-18 21:15:01 -0300

    Exercício 16: Escreva um programa que determina o maior entre três números.

---
+++ exercicio16.js
@@ -0,0 +1,9 @@
+//Exercício 16: Escreva um programa que determina o maior entre três números.
+let numero1 = 10;
+let numero2 = 1;
+let numero3 = 200;
+
+let maior = Math.max(numero1, numero2, numero3);
+
+console.log(`O maior número é ${maior}`);
+
