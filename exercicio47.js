81b18feedf3a471bda1d3a1e853818ca77039724
Vitória <104187728+Vitoria-Araujo@users.noreply.github.com>
2025-09-21 21:36:33 -0300

    Exercício 47: Escreva uma função que aceite um número como argumento e retorne o seu quadrado.

---
+++ exercicio47.js
@@ -0,0 +1,7 @@
+//Exercício 47: Escreva uma função que aceite um número como argumento e retorne o seu quadrado.
+
+function numQuadrado(num1 ){
+    return num1 ** 2
+}
+
+console.log(numQuadrado(10));
