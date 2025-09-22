3fd70476433418f4c45877691f50717d2149ebe4
Vitória <104187728+Vitoria-Araujo@users.noreply.github.com>
2025-09-21 21:52:32 -0300

    Exercício 49: Escreva uma função que aceite três números como argumentos e retorne o maior deles.

---
+++ exercicio49.js
@@ -0,0 +1,6 @@
+//Exercício 49: Escreva uma função que aceite três números como argumentos e retorne o maior deles.
+function maiorNumero(num1, num2, num3){
+      return Math.max(num1, num2, num3);
+}
+
+console.log(maiorNumero(2, 4, 100));
