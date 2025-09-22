5c359eac9b39ce5eae68a21244df5f704e61c9fe
Vitória <104187728+Vitoria-Araujo@users.noreply.github.com>
2025-09-21 21:31:49 -0300

    Exercício 46: Escreva uma função que aceite dois números como argumentos e retorne sua soma.

---
+++ exercicio46.js
@@ -0,0 +1,7 @@
+//Exercício 46: Escreva uma função que aceite dois números como argumentos e retorne sua soma.
+
+function somaDoisNumeros(num1, num2){
+    return num1 + num2;
+}
+
+console.log(somaDoisNumeros(5, 10));
